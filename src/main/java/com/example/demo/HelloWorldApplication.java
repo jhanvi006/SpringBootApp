d4809efc1774aa01630c460a2e8459be529a699b
jhanvi006 <jhanvikankhara06@gmail.com>
2022-07-05 20:57:09 +0530

    Added: welcome message on spring boot app added.

---
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz!");
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
